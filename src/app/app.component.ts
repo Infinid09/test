72a9c39c1f1b78a75c5ed75ca3129900954938b4
Infinid09 <umangdhiman17@gmail.com>
2018-01-11 00:21:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import Imdb = require('imdb-api');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // imdb = require('imdb-api');
+  // Imdb
+  constructor(){
+    const imdb = require('imdb-api');
+    imdb.get('The T', {apiKey: 'PlsBanMe', timeout: 30000}).then(console.log).catch(console.log);
+  }
+  
 }
